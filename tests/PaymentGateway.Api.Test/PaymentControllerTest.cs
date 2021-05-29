5d193b370e071bcd713e42be2158401140909436
Bruno Almeida <ext.bruno.almeida@farfetch.com>
2021-05-29 20:15:14 +0100

    Remove unused test

--- tests/PaymentGateway.Api.Test/PaymentControllerTest.cs
+++ tests/PaymentGateway.Api.Test/PaymentControllerTest.cs
@@ -47,38 +47,5 @@ namespace PaymentGateway.Api.Test
             Assert.Equal(200, result?.StatusCode);
             mediator.Verify(m => m.Send(It.IsAny<PaymentCommand>(), CancellationToken.None), Times.Once);
         }
-        
-        [Fact(DisplayName = "A internal error happens should return Bad Request status code")]
-        public async Task AddNewPaymentShouldReturnBadRequest()
-        {
-            var response = new PaymentResult
-            {
-                Id = Guid.NewGuid(),
-                Status = PaymentStatus.Error
-            };
-
-            var mediator = new Mock<IMediator>();
-            mediator.Setup(m => m.Send(It.IsAny<PaymentCommand>(), CancellationToken.None)).ReturnsAsync(response);
-
-            var log = new Mock<ILogger<PaymentController>>();
-            var controller = new PaymentController(log.Object, mediator.Object);
-
-            var request = new Payment
-            {
-                Amount = 200,
-                CreditCard = new CreditCard
-                {
-                    Name = "Natalie Buckley",
-                    Number = "4088043836019395",
-                    Month = 8,
-                    Year = 2030,
-                    CVV = "159"
-                }
-            };
-
-            var result = await controller.AddPayment(request) as BadRequestObjectResult;
-            Assert.Equal(400, result?.StatusCode);
-            mediator.Verify(m => m.Send(It.IsAny<PaymentCommand>(), CancellationToken.None), Times.Once);
-        }
     }
 }
